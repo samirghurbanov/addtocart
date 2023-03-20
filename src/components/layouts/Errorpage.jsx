c6f84eff794a4b0dd6ce5cc2aac3077746fe13e2
Samir Qurbanov <qurbanovsamir111@gmail.com>
2023-03-20 01:17:28 -0700

    error duzeldi

--- src/components/layouts/Errorpage.jsx
+++ src/components/layouts/Errorpage.jsx
@@ -11,7 +11,7 @@ const Errorpage = () => {
       <div class="row">
         <div class="col-sm-8 offset-sm-2 text-center text-white">
           <h1 class=""><span>4</span><span>0</span><span>4</span></h1>
-          <h5 class="">Oops! Page not found</h5>
+          <h5 class="">Oops! Page not foundd</h5>
           <p class="mb-4">we are sorry, but the page you requested was not found</p>
           <Link class="btn btn-dark" to={'/'} title="home url">
             Go home
